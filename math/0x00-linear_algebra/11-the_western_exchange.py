547ce7d30a0a22f70d1ab2bbc07adebd600d4acb
cbarros7 <cj.barros07@gmail.com>
2021-07-15 01:13:58 -0500

    Add task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""transposes matrix"""
+
+
+def np_transpose(matrix):
+    """np_transpose: transposes matrix"
+
+    Args:
+        matrix: matrix to transpose
+    """
+    return matrix.T

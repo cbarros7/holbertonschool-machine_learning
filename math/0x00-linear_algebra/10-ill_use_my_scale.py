4f95849eff1b7511e1492a855084a8765a17cbb9
cbarros7 <cj.barros07@gmail.com>
2021-07-15 01:11:27 -0500

    Fix task 10

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -8,14 +8,4 @@ def np_shape(matrix):
     Args:
         matrix: matrix to calculate the shape
     """
-    _shape = []
-    if len(matrix.tolist()) == 0:
-        _shape.append(0)
-    elif isinstance(matrix.tolist()[0], list):
-        _shape.append(len(matrix))
-        _shape.append(len(matrix[0]))
-        _shape.append(len(matrix[0][0]))
-    else:
-        _shape.append(len(matrix))
-
-    return tuple(_shape)
+    return matrix.shape

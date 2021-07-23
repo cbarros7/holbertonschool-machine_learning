f001b4d4ce050deee4ab65e836c9685c6d0b523a
cbarros7 <cj.barros07@gmail.com>
2021-07-22 19:54:22 -0500

    Add derivate and integrate

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" function that calculates derivative of a polynomial """
+
+
+def poly_derivative(poly):
+    """ find the derivative of  a polynomial
+        poly: list with the plynomial
+        Return: list with the polynomial of the derivative
+    """
+    result = []
+
+    if poly is None or type(poly) != list or poly == []:
+        return None
+
+    for i in range(len(poly)):
+        if type(poly[i]) not in (int, float):
+            return None
+        elif len(poly) == 1:
+            result.append(0)
+        else:
+            if i == 0:
+                continue
+            result.append(i * poly[i])
+
+    return result

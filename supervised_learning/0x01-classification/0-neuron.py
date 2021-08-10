d6b099c92629d6086e476761c06ca19ed754c3be
cbarros7 <cj.barros07@gmail.com>
2021-08-09 23:57:40 -0500

    Fix pydcodestyle

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -2,15 +2,17 @@
 """Classification"""
 import numpy as np
 
-class Neuron():
-  """Single neuron performing binary classification"""
-  def __init__(self, nx):
-    """Constructor"""
-    if not isinstance(nx, int):
-      raise TypeError("nx must be an integer")
-    if nx < 1:
-      raise ValueError("nx must be a positive integer")
 
-    self.W =  np.random.normal(0, 1, (1, nx))
-    self.b = 0 
-    self.A = 0
+class Neuron():
+    """Single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(0, 1, (1, nx))
+        self.b = 0
+        self.A = 0
--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -2,42 +2,44 @@
 """Classification"""
 import numpy as np
 
+
 class Neuron():
-  """Single neuron performing binary classification"""
-  def __init__(self, nx):
-    """Constructor"""
-    if not isinstance(nx, int):
-      raise TypeError("nx must be an integer")
-    if nx < 1:
-      raise ValueError("nx must be a positive integer")
+    """Single neuron performing binary classification"""
 
-    self.__W =  np.random.normal(0, 1, (1, nx))
-    self.__b = 0 
-    self.__A = 0
+    def __init__(self, nx):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
 
-  @property
-  def W(self):
-    """W: The weights vector for the neuron.
+        self.__W = np.random.normal(0, 1, (1, nx))
+        self.__b = 0
+        self.__A = 0
 
-    Returns:
-        W: int
-    """
-    return self.__W
-    
-  @property
-  def b(self):
-    """b: The bias for the neuron. Upon instantiation
+    @property
+    def W(self):
+        """W: The weights vector for the neuron.
 
-    Returns:
-        b: int
-    """
-    return self.__b
+        Returns:
+            W: int
+        """
+        return self.__W
 
-  @property
-  def A(self):
-    """A: The activated output of the neuron (prediction).
+    @property
+    def b(self):
+        """b: The bias for the neuron. Upon instantiation
 
-    Returns:
-        A: int
-    """
-    return self.__A
+        Returns:
+            b: int
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """A: The activated output of the neuron (prediction).
+
+        Returns:
+            A: int
+        """
+        return self.__A

bba40176e78f07129139efd95e5f622a6fb849fe
cbarros7 <cj.barros07@gmail.com>
2021-08-10 23:57:57 -0500

    Modify task 6

--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -10,7 +10,7 @@ class Neuron():
         """Constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx < 1: 
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
 
         self.__W = np.random.normal(0, 1, (1, nx))
@@ -30,7 +30,14 @@ class Neuron():
         return self.__A
 
     def forward_prop(self, X):
-        """forward propagation function"""
+        """forward_prop: Calculates the forward propagation of the neuron
+
+        Args:
+            X : with shape (nx, m) that contains the input data
+
+        Returns:
+            the private attribute __A
+        """
         Z = np.matmul(self.W, X) + self.b
         self.__A = self.sigmoid(Z)
         return self.A
@@ -40,20 +47,57 @@ class Neuron():
         return 1 / (1 + np.exp(-1 * Y))
 
     def cost(self, Y, A):
-        """defnine the cost function"""
+        """cost: defnine the cost function
+
+        Args:
+            Y : with shape (1, m) that contains the correct
+                labels for the input data
+            A : containing the activated output of the neuron for each example
+
+        Returns:
+            the cost
+        """
         # classification pb: use the cross-entropy function
         m = Y.shape[1]
         return (-1 / m) * np.sum(
             Y * np.log(A) + (1 - Y) * (np.log(1.0000001 - A)))
 
     def evaluate(self, X, Y):
-        """function that evaluates the neuron's predictions"""
+        """evaluate: evaluates the neuron's predictions
+
+        Args:
+            X : with shape (nx, m) that contains the input data
+                nx is the number of input features to the neuron
+                m is the number of examples
+
+            Y : with shape (1, m) that contains the
+                correct labels for the input data
+
+        Returns:
+            the neuron’s prediction and the cost of the network
+        """
         A = self.forward_prop(X)
         cost = self.cost(Y, A)
         return np.where(A >= 0.5, 1, 0), cost
 
     def gradient_descent(self, X, Y, A, alpha=0.05):
-        """function that calculates one pass of gradient descent"""
+        """gradient_descent: function that calculates
+                            one pass of gradient descent
+
+        Args:
+            X : with shape (nx, m) that contains the input data
+                nx is the number of input features to the neuron
+                m is the number of examples
+
+            Y : with shape (1, m) that contains the correct
+                labels for the input data
+
+            A : with shape (1, m) containing the activated
+                output of the neuron for each example
+
+            alpha : the learning rate. Defaults to 0.05.
+        """
+
         dZ = A - Y
         m = Y.shape[1]
         dW = (1 / m) * np.matmul(dZ, X.T)

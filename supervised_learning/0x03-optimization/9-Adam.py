fb13639b9b1b0bc7403ba68261a63178e8def9a5
cbarros7 <cj.barros07@gmail.com>
2021-08-18 21:59:13 -0500

    Fix task 9

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python3
 """Adam"""
-import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
-    """function that implements the Adam optimization algorithm"""
-    v = beta1 * v + (1 - beta1) * grad
-    # introduce bias correction
-    v_corr = v / (1 - (beta1 ** t))
-    s = beta2 * s + (1 - beta2) * (grad ** 2)
-    # introduce bias correction
-    s_corr = s / (1 - (beta2 ** t))
-    var -= alpha * (v_corr / (np.sqrt(s_corr) + epsilon))
-    return var, v, s
+    """update_variables_Adam: updates a variable in place using
+                            the Adam optimization algorithm
+
+    Args:
+        alpha is the learning rate
+        beta1 is the weight used for the first moment
+        beta2 is the weight used for the second moment
+        epsilon is a small number to avoid division by zero
+        var is a numpy.ndarray containing the variable to be updated
+        grad is a numpy.ndarray containing the gradient of var
+        v is the previous first moment of var
+        s is the previous second moment of var
+        t is the time step used for bias correction.
+
+    Returns:
+        the updated variable, the new first moment,
+        and the new second moment, respectively
+    """
+    α = alpha
+    β1 = beta1
+    β2 = beta2
+    ε = epsilon
+
+    Vd = (β1 * v) + ((1 - β1) * grad)
+    Sd = (β2 * s) + ((1 - β2) * grad * grad)
+
+    Vd_ok = Vd / (1 - β1 ** t)
+    Sd_ok = Sd / (1 - β2 ** t)
+
+    w = var - α * (Vd_ok / ((Sd_ok ** (0.5)) + ε))
+    return (w, Vd, Sd)

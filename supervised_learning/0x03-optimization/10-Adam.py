90fc6dc142c82005672c9129f38f588373cc6bb8
cbarros7 <cj.barros07@gmail.com>
2021-08-18 21:32:40 -0500

    Add task 10

---
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Adam"""
+import tensorflow as tf
+import numpy as np
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """create_Adam_op: creates the training operation for a neural network
+                    in tensorflow using the Adam optimization algorithm
+    Args:
+        loss is the loss of the network
+        alpha is the learning rate
+        beta1 is the weight used for the first moment
+        beta2 is the weight used for the second moment
+        epsilon is a small number to avoid division by zero
+
+    Returns: the Adam optimization operation
+    """
+    return tf.train.AdamOptimizer(
+        learning_rate=alpha, beta1=beta1, beta2=beta2,
+        epsilon=epsilon, use_locking=False,
+        name='Adam')

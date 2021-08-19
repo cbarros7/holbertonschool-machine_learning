6aa5d42c1632771ad934c37dcf2ee5e5aaec19ea
cbarros7 <cj.barros07@gmail.com>
2021-08-18 21:51:47 -0500

    Fix pycodestyle task 9 and 10

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
-"""Adam"""
+"""Adam Upgraded"""
 import tensorflow as tf
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
-    """create_Adam_op: creates the training operation for a neural network
-                        in tensorflow using the Adam optimization algorithm
-    Args:
-        loss is the loss of the network
-        alpha is the learning rate
-        beta1 is the weight used for the first moment
-        beta2 is the weight used for the second moment
-        epsilon is a small number to avoid division by zero
-
-    Returns: the Adam optimization operation
-    """
-    return tf.train.AdamOptimizer(learning_rate=alpha,
-                                  beta1=beta1, beta2=beta2,
-                                  epsilon=epsilon,
-                                  use_locking=False,
-                                  name='Adam').minimize(loss)
+    """function that implements Adam gradient descent in tensorflow"""
+    return tf.train.AdamOptimizer(
+        learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon,
+        use_locking=False, name='Adam'
+    ).minimize(loss)
--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -4,24 +4,7 @@ import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
-    """update_variables_Adam: updates a variable in place using
-                            the Adam optimization algorithm
-
-    Args:
-        alpha is the learning rate
-        beta1 is the weight used for the first moment
-        beta2 is the weight used for the second moment
-        epsilon is a small number to avoid division by zero
-        var is a numpy.ndarray containing the variable to be updated
-        grad is a numpy.ndarray containing the gradient of var
-        v is the previous first moment of var
-        s is the previous second moment of var
-        t is the time step used for bias correction
-
-    Returns:
-        the updated variable, the new first moment,
-        and the new second moment, respectively
-    """
+    """function that implements the Adam optimization algorithm"""
     v = beta1 * v + (1 - beta1) * grad
     # introduce bias correction
     v_corr = v / (1 - (beta1 ** t))

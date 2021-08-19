228c11c55e92c27e04741ef43aeaa65f2c2d6a04
cbarros7 <cj.barros07@gmail.com>
2021-08-18 20:54:04 -0500

    Add task 6 and 7

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -4,6 +4,18 @@ import tensorflow as tf
 
 
 def create_momentum_op(loss, alpha, beta1):
+    """create_momentum_op: creates the training operation for a
+                            neural network in tensorflow using the gradient
+                            descent with momentum optimization algorithm
+
+    Args:
+        loss is the loss of the network
+        alpha is the learning rate
+        beta1 is the momentum weight
+
+    Returns:
+        the momentum optimization operation
+    """
     return tf.train.MomentumOptimizer(
         learning_rate=alpha, momentum=beta1, use_locking=False,
         name='Momentum', use_nesterov=False

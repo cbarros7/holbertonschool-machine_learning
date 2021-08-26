35c59203112cda803bcbd85ecbdcd1603cc2eab8
cbarros7 <cj.barros07@gmail.com>
2021-08-26 17:32:18 -0500

    Add task 1 and 2

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
-"""L2 Regularization Cost"""
-import tensorflow as tf
+"""Gradient Descent with L2 Regularization"""
+import numpy as np
 
 
-def l2_reg_cost(cost):
-    """function that calculates the cost of a nn with L2 regularization"""
-    return cost + tf.losses.get_regularization_losses()
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """function that updates the weights and biases of a nn using
+        gradient descent with L2 regularization"""
+    weights_copy = weights.copy()
+    for i in range(L, 0, -1):
+        m = Y.shape[1]
+        if i != L:
+            # all layers use a tanh activation, except last
+            # introduce call to tanh_prime method
+            dZi = np.multiply(np.matmul(
+                weights_copy['W' + str(i + 1)].T, dZi
+            ), 1 - cache['A' + str(i)] ** 2)
+        else:
+            # last layer uses a softmax activation
+            dZi = cache['A' + str(i)] - Y
+        dWi = np.matmul(dZi, cache['A' + str(i - 1)].T) / m
+        dbi = np.sum(dZi, axis=1, keepdims=True) / m
+
+        l2 = (1 - alpha * lambtha / m)
+        weights['W' + str(i)] = l2 * weights_copy['W' + str(i)] - alpha * dWi
+        weights['b' + str(i)] = weights_copy['b' + str(i)] - alpha * dbi

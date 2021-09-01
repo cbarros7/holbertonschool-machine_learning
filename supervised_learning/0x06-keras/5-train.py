4fd3bb30861d223151b32b4d4b80b053a9b2bb6e
cbarros7 <cj.barros07@gmail.com>
2021-08-31 22:03:14 -0500

    Fix task 5

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -5,7 +5,7 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
-    """"function that trains a model using mini-batch gradient descent""""
+    """"function that trains a model using mini-batch gradient descent"""
     history = network.fit(x=data, y=labels,
                           batch_size=batch_size,
                           epochs=epochs,

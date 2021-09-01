586fc8e9ffee7ed87046c716fbc1fac01679581a
cbarros7 <cj.barros07@gmail.com>
2021-08-31 21:55:25 -0500

    Add task 3 to 5

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Train a model, with validaiton data"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """"function that trains a model using mini-batch gradient descent""""
+    history = network.fit(x=data, y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle,
+                          validation_data=validation_data)
+    return history

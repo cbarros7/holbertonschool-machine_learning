c99ed0b31c4d6181b5ebffff1a88e7e99629a7f3
cbarros7 <cj.barros07@gmail.com>
2021-08-31 22:01:45 -0500

    Add task 6 to 8

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Train a model, with early stopping"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """function that trains a model using mini-batch gradient descent"""
+    callbacks = []
+    if validation_data and early_stopping:
+        # The patience parameter is the number of epochs
+        # upon which improvement should be checked
+        early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                               patience=patience)
+        callbacks.append(early_stop)
+    history = network.fit(x=data, y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle,
+                          validation_data=validation_data,
+                          callbacks=callbacks)
+    return history
---
+++ supervised_learning/0x06-keras/7-train.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Train a model, with learning rate decay"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
+    """function that trains a model using mini-batch gradient descent"""
+
+    callbacks = []
+
+    if validation_data and early_stopping:
+        # The patience parameter is the number of epochs
+        # upon which improvement should be checked
+        early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                               patience=patience)
+        callbacks.append(early_stop)
+
+    if validation_data and learning_rate_decay:
+
+        def schedule(epoch):
+            """
+            function that takes an epoch index as input (integer,
+            indexed from 0) and returns a new learning rate as output (float)
+            """
+            return alpha / (1 + decay_rate * epoch)
+
+        lr_decay = K.callbacks.LearningRateScheduler(
+            schedule=schedule, verbose=1)
+        callbacks.append(lr_decay)
+
+    history = network.fit(x=data, y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle,
+                          validation_data=validation_data,
+                          callbacks=callbacks)
+    return history

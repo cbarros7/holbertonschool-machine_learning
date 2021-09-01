7747e03ebdfcb0999dff6cff725ac0c0849454ff
cbarros7 <cj.barros07@gmail.com>
2021-08-31 22:26:03 -0500

    Fix task 12

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 """Test"""
+
 import tensorflow.keras as K
 
 
 def test_model(network, data, labels, verbose=True):
     """function that tests a neural network"""
-    result = network.evaluate(
-        x=data,
-        y=labels,
-        batch_size=None,
-        verbose=verbose
-    )
-    return result
+    return network.evaluate(data, labels, verbose=verbose)

9112aee590ba81e25ed2965c2d2d5d6c0a1e99dc
cbarros7 <cj.barros07@gmail.com>
2021-10-10 23:15:59 -0500

    Fix pycodestyle task 0

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -3,14 +3,15 @@
 import numpy as np
 import tensorflow as tf
 
+
 class Yolo():
-  """Class YOLO"""
-  
-  def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-    """define and initialize attributes and variables"""
-    self.model = tf.keras.models.load_model(model_path)
-    with open(classes_path, 'r') as f:
-      self.class_names = [line.strip() for line in f]
-    self.class_t = class_t
-    self.nms_t = nms_t
-    self.anchors = anchors
+    """Class YOLO"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """define and initialize attributes and variables"""
+        self.model = tf.keras.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors

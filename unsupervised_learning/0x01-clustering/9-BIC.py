574ce7a10f384a348c1870137e9d9ea60a723aba
cbarros7 <cj.barros07@gmail.com>
2022-03-30 18:12:48 -0500

    Fix task 9

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,58 +1,83 @@
 #!/usr/bin/env python3
-"""
-9-BIC.py
-"""
+""" clustering """
 import numpy as np
+
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
-    function that finds the best number of clusters for a GMM using
-    the Bayesian Information Criterion
+    Function that performs the expectation maximization for a GMM:
+    Args:
+                X: numpy.ndarray -> Array of shape (n, d) with the data
+        kmin: int -> positive int with the minimum number
+            of clusters to check for (inclusive)
+        kmax: int -> positive int with the maximum number
+            of clusters to check for (inclusive)
+        iterations: int -> positive int with the maximum number
+            of iterations for the algorithm
+        tol: float -> non-negative float with the tolerance
+            of the log likelihood, used to
+            determine early stopping i.e. if the
+            difference is less than or equal to
+            tol you should stop the algorithm
+        verbose: bool -> boolean that determines if you should
+            print information about the algorithm
+            If True, print Log Likelihood after {i} iterations: {l}
+            every 10 iterations and after the last iteration
+                {i} is the number of iterations of the EM algorithm
+                 {l} is the log likelihood
+            You should use:
+            initialize = __import__('4-initialize').initialize
+            expectation = __import__('6-expectation').expectation
+            maximization = __import__('7-maximization').maximization
     """
 
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
-    if not isinstance(kmin, int) or kmin <= 0 or X.shape[0] <= kmin:
+    if kmax is None:
+        kmax = X.shape[0]
+    if type(kmin) != int or kmin <= 0 or X.shape[0] <= kmin:
         return None, None, None, None
-    if not isinstance(kmax, int) or kmax <= 0 or X.shape[0] <= kmax:
+    if type(kmax) != int or kmax <= 0 or X.shape[0] < kmax:
         return None, None, None, None
-    if not isinstance(iterations, int) or iterations <= 0:
+    if kmax <= kmin:
         return None, None, None, None
-    if not isinstance(tol, float) or tol < 0:
+    if type(iterations) != int or iterations <= 0:
         return None, None, None, None
-    if not isinstance(verbose, bool):
+    if type(tol) != float or tol < 0:
+        return None, None, None, None
+    if type(verbose) != bool:
         return None, None, None, None
 
-    # X: array of shape (n, d) containing the data set
     n, d = X.shape
 
-    # Define pi_t, m_t, S_t: arrays containing the relevant
-    # parameters for all the clusters
-    all_pis = []
-    all_ms = []
-    all_Ss = []
-    all_lkhds = []
-    all_bs = []
+    b = []
+    results = []
+    ks = []
+    l_ = []
 
-    # Iterate over the ((kmax + 1) - kmin) clusters
     for k in range(kmin, kmax + 1):
-        pi, m, S, g, lkhd = expectation_maximization(X, k, iterations,
-                                                     tol, verbose)
-        all_pis.append(pi)
-        all_ms.append(m)
-        all_Ss.append(S)
-        all_lkhds.append(lkhd)
-        # p: the number of parameters required for the model
-        p = (k * d * (d + 1) / 2) + (d * k) + (k - 1)
-        # b: array containing the BIC value for each cluster size tested
-        b = p * np.log(n) - 2 * lkhd
-        all_bs.append(b)
+        ks.append(k)
 
-    all_lkhds = np.array(all_lkhds)
-    all_bs = np.array(all_bs)
-    best_k = np.argmin(all_bs)
-    best_result = (all_pis[best_k], all_ms[best_k], all_Ss[best_k])
+        pi, m, S, g, l_k = expectation_maximization(X,
+                                                    k,
+                                                    iterations=iterations,
+                                                    tol=tol,
+                                                    verbose=verbose)
+        results.append((pi, m, S))
 
-    return best_k+1, best_result, all_lkhds, all_bs
+        l_.append(l_k)
+        p = k - 1 + k * d + k * d * (d + 1) / 2
+
+        bic = p * np.log(n) - 2 * l_k
+        b.append(bic)
+
+    l_ = np.array(l_)
+    b = np.array(b)
+
+    index = np.argmin(b)
+    best_k = ks[index]
+    best_result = results[index]
+
+    return best_k, best_result, l_, b
